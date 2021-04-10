87c25e9d1fea4c4900e50cb64d62b1deecb3c02a
superMinecraftL <Leonard@tussilago.eu>
2021-04-10 13:59:30 +0700

    wip

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ ClientMultiSocket = socket.socket()
 host = '127.0.0.1'
 port = 2004
 
+username = input("What is your username?")
 print('Waiting for connection response')
 try:
     ClientMultiSocket.connect((host, port))
 except socket.error as e:
     print(str(e))
 
+ClientMultiSocket.send("HELO " + username)
 res = ClientMultiSocket.recv(1024)
+print(res.decode('utf-8'))
 while True:
     Input = input('Hey there: ')
     ClientMultiSocket.send(str.encode(Input))
