ddb3ed7f942460b395b86872744b538900dadd4a
arduino5144 <leonard@tussilago.eu>
2021-05-02 12:08:32 +0700

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import socket
 
-ClientMultiSocket = socket.socket()
+def receive_and_print_all_data(connection):
+    connection.setblocking(1)
+    data = connection.recv(1024)
+    connection.setblocking(0)
+    while len(data) > 1:
+        print(data.decode('utf-8'), end='')
+        data = connection.recv(1024)
+    print()
+    connection.setblocking(1)
+
+client_multi_socket = socket.socket()
 host = '127.0.0.1'
 port = 2004
 
@@ -8,15 +18,19 @@ username = input("What is your username?")
 password = input("What's the secret password?")
 print('Waiting for connection response')
 try:
-    ClientMultiSocket.connect((host, port))
+    client_multi_socket.connect((host, port))
 except socket.error as e:
     print(str(e))
 
-ClientMultiSocket.send(str.encode("HELO " + username + ' ' + password))
-res = ClientMultiSocket.recv(1024)
+client_multi_socket.send(str.encode("HELO " + username + ' ' + password))
+res = client_multi_socket.recv(1024)
 print(res.decode('utf-8'))
 while True:
     Input = input('Hey there: ')
-    ClientMultiSocket.send(str.encode(Input))
-    res = ClientMultiSocket.recv(1024)
-    print(res.decode('utf-8'))
+    client_multi_socket.send(str.encode(Input))
+    receive_and_print_all_data()
+    # res = client_multi_socket.recv(1024)
+    # print(res.decode('utf-8'))
+    # res = client_multi_socket.recv(1024)
+    # print(res.decode('utf-8'))
+
